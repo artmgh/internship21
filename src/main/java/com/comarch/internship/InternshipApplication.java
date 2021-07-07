b1a36ca0605346bea3611c9cb0fe0d6c2b56e01f
amiksa <artur.miksa@comarch.com>
2021-07-07 09:47:07 +0200

    display beans

--- src/main/java/com/comarch/internship/InternshipApplication.java
+++ src/main/java/com/comarch/internship/InternshipApplication.java
@@ -1,15 +1,26 @@
 package com.comarch.internship;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class InternshipApplication {
 
+    private static ApplicationContext applicationContext;
+
 
     public static void main(String[] args) {
-        SpringApplication.run(InternshipApplication.class, args);
+        applicationContext = SpringApplication.run(InternshipApplication.class, args);
+        displayAllBeans();
     }
 
+    public static void displayAllBeans() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : allBeanNames) {
+            System.out.println("BEAN: " + beanName);
+        }
+    }
 
 }
